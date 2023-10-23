66cdca3d21b5ea4776e64cba9444ff1ca810dbae
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2023-10-23 23:32:08 +0900

    Update: Babel設定のカスタマイズ: エイリアスの設定とリアクティブプラグインの追加

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    // ルートフォルダを設定。これにより、絶対パスでのインポートが可能になります。
+                    root: ['./src'],
+                    extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.json', '.ts', '.tsx'],
+                    // エイリアスの設定。これにより、指定したキーワードで特定のパスを参照できるようになります。
+                    alias: {
+                        // 他のエイリアスもここに追加
+                        "@": "./src",
+                        '@components': './src/components',
+                        '@assets': './src/assets',
+                        '@styles': './src/styles',
+                        '@libs': './src/libs',
+                        '@screens': './src/screens',
+                        // ... その他必要なエイリアス
+                    },
+                },
+            ],
+            'react-native-reanimated/plugin',
+        ],
+    };
 };
