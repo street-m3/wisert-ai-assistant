317e6b2c04c6bd4ee1281d65e7c0427967453626
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2023-10-28 19:12:21 +0900

    Add: ドロワーナビゲーションとスプラッシュスクリーンの実装
    1. ドロワーナビゲーションの導入:
       - '@react-navigation/drawer' を利用して、ドロワーナビゲーションをアプリに統合。
       - ナビゲーションに 'Home' と 'Settings' の二つのスクリーンを設定。
    
    2. スプラッシュスクリーンの最適化:
       - 'expo-splash-screen' を使用して、アプリのロード時にスプラッシュスクリーンが自動的に非表示になるのを防ぎます。
       - スプラッシュスクリーンを3秒間表示した後、非表示にすることで、ユーザーにスムーズなスタート体験を提供。
    
    3. その他の改善:
       - 'native-base' の 'NativeBaseProvider' を導入して、UIコンポーネントに一貫したテーマを適用。
       - 'react-native-gesture-handler' のインポートを追加して、タッチジェスチャーの処理を最適化。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Homepage from '@screens/Home';
 import Settings from '@screens/Settings';
 import { registerRootComponent } from 'expo';
-import { preventAutoHideAsync, hideAsync } from 'expo-splash-screen';
-import { Text, View } from 'react-native';
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
+import { NativeBaseProvider } from 'native-base';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import CustomizeDrawer from './components/CustomMenu';
 
@@ -16,37 +16,37 @@ const App = (): JSX.Element => {
         const prepareResource = async () => {
             try {
                 await preventAutoHideAsync();
-                await new Promise(resolve => setTimeout(resolve, 3000));
+                await new Promise((resolve) => setTimeout(resolve, 3000));
             } catch (e) {
                 console.error(e);
             } finally {
-                await hideAsync()
+                await hideAsync();
             }
-        }
-        prepareResource()
-    }, [])
+        };
+        prepareResource();
+    }, []);
 
     return (
-        <NavigationContainer>
-            <Drawer.Navigator
-                initialRouteName="Home"
-                screenOptions={{ drawerType: 'front' }}
-                drawerContent={(props) => 
-                    <CustomizeDrawer {...props} />
-                }
-            >
-                <Drawer.Screen
-                    name="Home"
-                    options={{ drawerLabel: 'Homepage', title: 'Homepage' }}
-                    component={Homepage}
-                />
-                <Drawer.Screen
-                    name="Settings"
-                    options={{ drawerLabel: 'Settings', title: 'Settings' }}
-                    component={Settings}
-                />
-            </Drawer.Navigator>
-        </NavigationContainer>
+        <NativeBaseProvider>
+            <NavigationContainer>
+                <Drawer.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{ drawerType: 'front' }}
+                    drawerContent={(props) => <CustomizeDrawer {...props} />}
+                >
+                    <Drawer.Screen
+                        name="Home"
+                        options={{ drawerLabel: 'Homepage', title: 'Homepage' }}
+                        component={Homepage}
+                    />
+                    <Drawer.Screen
+                        name="Settings"
+                        options={{ drawerLabel: 'Settings', title: 'Settings' }}
+                        component={Settings}
+                    />
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </NativeBaseProvider>
     );
 };
 
