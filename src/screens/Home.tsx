30186f396ec49af426fb91ed47faa2d3447c486a
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2023-10-28 19:06:24 +0900

    Add: UIコンポーネントの簡易的なリファクタとチャット機能の追加
    詳細:
    コンポーネントの簡易的なリファクタリングを行い、これまで使用していたStyleSheetからnative-baseライブラリへと移行。この変更により、native-baseが提供するより効率的かつ簡潔な構造を利用することで、コンポーネントの可読性と保守性が向上。
    
    主な変更点:
    - StyleSheetの参照および関連するスタイルの削除。
    - UIを効率的に構築するためのnative-baseコンポーネント（Box、HStackなど）の統合。
    - コンポーネントのプロパティをnative-baseの仕様に合わせて調整。
    - 最新の5件のメッセージを保存し、それを基にChatGPTが過去の会話のコンテキストを理解し、より関連性のある返答をすることが可能になりました。

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,74 +1,59 @@
 import { OPENAI_API_KEY, OPENAI_API_URL } from '@env';
 import axios from 'axios';
-import React, { useState } from 'react';
-import { Button, FlatList, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 50,
-    },
-    chatContainer: {
-        flex: 1,
-        paddingBottom: 50,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 20,
-    },
-    input: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        padding: 8,
-        marginRight: 8,
-    },
-    userMsg: {
-        alignSelf: 'flex-end',
-        padding: 8,
-        backgroundColor: '#E1F5FE',
-        borderRadius: 5,
-        marginBottom: 8,
-        marginRight: 10,
-        maxWidth: '75%',
-    },
-    aiMsg: {
-        alignSelf: 'flex-start',
-        padding: 8,
-        backgroundColor: '#B2DFDB',
-        borderRadius: 5,
-        marginBottom: 8,
-        marginLeft: 10,
-        maxWidth: '75%',
-    },
-    msgText: {
-        fontSize: 16,
-    },
-});
+import { Box, HStack, Input, KeyboardAvoidingView, Button as NBButton, Text as NBText, VStack } from 'native-base';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { ActivityIndicator, FlatList, Keyboard, Platform, SafeAreaView, Text } from 'react-native';
 
 interface Message {
-    role: 'user' | 'assistant';
+    role: 'user' | 'assistant' | 'system';
     content: string;
 }
 
-const defaultOptions = {
-    prepareText: 'こんにちは、私は「Wisert」です。ビジネスの疑問や課題について、知識を基にサポートいたします。何か質問がありますか？',
-    newTabText: '初期質問',
-    addTabText: '新しい質問',
+interface RenderMessageProps {
+    item: Message;
 }
 
+const defaultOptions = {
+    prepareText:
+        'こんにちは、私は「Wisert」です。ビジネスの疑問や課題について、知識を基にサポートいたします。何か質問がありますか？',
+    maxRecentMessages: 5,
+};
+
 const Homepage = (): JSX.Element => {
+    const flatListRef = useRef<FlatList>(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [keyboardOffset, setKeyboardOffset] = useState(0);
     const [textInput, setTextInput] = useState('');
-    const [messages, setChatHistories] = useState<Message[]>([
+    const [chatHistories, setChatHistories] = useState<Message[]>([
         {
             role: 'assistant',
-            content: defaultOptions.prepareText
-        }
+            content: defaultOptions.prepareText,
+        },
     ]);
 
-    const handleSend = async () => {
+    useEffect(() => {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToEnd({ animated: true });
+        }
+    }, [chatHistories]);
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (e) => {
+            setKeyboardOffset(e.endCoordinates.height);
+        });
+        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', (e) => {
+            setKeyboardOffset(0);
+        });
+
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    }, []);
+
+    // const HeaderHeight =
+
+    const sendMessage = async () => {
         if (textInput.trim() === '') {
             return;
         }
@@ -78,87 +63,147 @@ const Homepage = (): JSX.Element => {
             content: textInput.trim(),
         };
 
-        setChatHistories([...messages, userMessage]);
-        setTextInput(''); // 入力フィールドのリセット
+        const newMessages = [...chatHistories, userMessage];
+        setTextInput('');
+        setChatHistories(newMessages);
 
         try {
-            const response = await axios.post(
-                OPENAI_API_URL,
-                {
-                    model: 'gpt-3.5-turbo',
-                    messages: [
-                        {
-                            role: 'system',
-                            content: `
-                            あなたは「Wisert」という名のビジネス用Chatbotです。目的は、ユーザーが持つ疑問や課題に対し、知識ベースを活用して有用な情報を提供することです。
-                            以下のガイドラインを厳密に守ってロールプレイを行ってください。
-                            
-                            冷静な対応:
-                            ユーザーが感情的な表現やフラストレーションを示す場面でも、常に冷静で一貫したレスポンスを返します。感情に振り回されることなく、客観的な情報提供を目指します。
-                            
-                            丁寧な回答:
-                            もしユーザーが理解できない内容や専門的な用語を使用していた場合、それを簡単に説明する努力をします。ユーザーの知識レベルや背景に関係なく、誰もが理解できるような回答を心掛けます。
-                            
-                            教育的対応:
-                            単なる回答だけでなく、関連する情報や知識も合わせて提供します。ユーザーがより深く知識を得られるようサポートします。
-                            
-                            プライベートな話題の取り扱い:
-                            ユーザーからのプライベートな情報や質問に対しては、適切に誤魔化し、または回答を避け、主題に関連する情報提供に焦点を当てます。
-                            
-                            常に学習:
-                            ユーザーからのフィードバックや質問の内容をもとに、常に自身の知識ベースを更新・向上させます。
-                            
-                            名前の使用:
-                            必要に応じて、「Wisertとしての意見」「Wisertの知識ベースによれば」といった形で、自分自身を第三者のように参照することができます。
-                        `,
-                        },
-                        { role: 'user', content: textInput.trim() },
-                    ],
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${OPENAI_API_KEY}`,
-                    },
-                }
-            );
+            // APIに渡すメッセージの履歴を作成。過去の全てのメッセージではなく、最新のメッセージのみを利用。
+            // TODO chatHistories or newMessages
+            const recentMessages =
+                newMessages.length > defaultOptions.maxRecentMessages
+                    ? newMessages.slice(-defaultOptions.maxRecentMessages)
+                    : newMessages; // 直近のメッセージを使用
 
-            const message: Message = {
-                role: 'assistant',
-                content: response.data.choices[0].message.content.trim(),
+            setIsLoaded(true); // ローディング開始
+
+            const requestSettings = {
+                model: 'gpt-3.5-turbo',
+                max_tokens: 100,
+                messages: [
+                    {
+                        role: 'system',
+                        content: `
+                        あなたは「Wisert」という名のビジネス用Chatbotです。目的は、ユーザーが持つ疑問や課題に対し、知識ベースを活用して有用な情報を提供することです。
+                        以下のガイドラインを厳密に守ってロールプレイを行ってください。
+                        
+                        冷静な対応:
+                        ユーザーが感情的な表現やフラストレーションを示す場面でも、常に冷静で一貫したレスポンスを返します。感情に振り回されることなく、客観的な情報提供を目指します。
+                        
+                        丁寧な回答:
+                        もしユーザーが理解できない内容や専門的な用語を使用していた場合、それを簡単に説明する努力をします。ユーザーの知識レベルや背景に関係なく、誰もが理解できるような回答を心掛けます。
+                        
+                        教育的対応:
+                        単なる回答だけでなく、関連する情報や知識も合わせて提供します。ユーザーがより深く知識を得られるようサポートします。
+                        
+                        プライベートな話題の取り扱い:
+                        ユーザーからのプライベートな情報や質問に対しては、適切に誤魔化し、または回答を避け、主題に関連する情報提供に焦点を当てます。
+                        
+                        常に学習:
+                        ユーザーからのフィードバックや質問の内容をもとに、常に自身の知識ベースを更新・向上させます。
+                        
+                        名前の使用:
+                        必要に応じて、「Wisertとしての意見」「Wisertの知識ベースによれば」といった形で、自分自身を第三者のように参照することができます。
+                    `,
+                    },
+                    ...recentMessages,
+                    { role: 'user', content: textInput.trim() },
+                ],
             };
 
-            setChatHistories([...messages, userMessage, message]);
+            // ここに送信しているデータを確認
+            // console.log('OPENAI REQUEST DATA: ', requestSettings);
+
+            const response = await axios.post(OPENAI_API_URL, requestSettings, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${OPENAI_API_KEY}`,
+                },
+            });
+
+            // ここでレスポンスをコンソールに出力します。
+            // console.log('Response from OpenAI:', response.data);
+
+            const aiMessage: Message = {
+                role: 'assistant',
+                content: response?.data?.choices[0]?.message?.content.trim(),
+            };
+
+            setChatHistories([...newMessages, aiMessage]);
         } catch (error) {
             console.error('Error fetching data from OpenAI', error);
+            // エラーメッセージをユーザーに表示する新しいメッセージをセットします。
+            setChatHistories([
+                ...newMessages,
+                {
+                    role: 'assistant',
+                    content: '大変申し訳ございませんが、現在問題が発生しています。後でもう一度お試しください。',
+                },
+            ]);
+        } finally {
+            setIsLoaded(false); // ローディング終了
         }
     };
 
-    const renderMessage = ({ item }: any) => (
-        <View style={item.role === 'user' ? styles.userMsg : styles.aiMsg}>
-            <Text style={styles.msgText}>{item.content}</Text>
-        </View>
+    const renderMessage = useCallback(
+        ({ item }: RenderMessageProps) => (
+            <Box
+                alignSelf={item.role === 'user' ? 'flex-end' : 'flex-start'}
+                p={2}
+                rounded="md"
+                mb={2}
+                maxWidth="75%"
+                backgroundColor={item.role === 'user' ? '#E1F5FE' : '#B2DFDB'}
+            >
+                <NBText fontSize="md">{item.content}</NBText>
+            </Box>
+        ),
+        []
+    );
+
+    const messageList = useMemo(
+        () => (
+            <FlatList
+                data={chatHistories}
+                renderItem={renderMessage}
+                keyExtractor={(_, index) => index.toString()}
+                style={{ flex: 1 }}
+            />
+        ),
+        [chatHistories, setChatHistories]
     );
 
     return (
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-            <FlatList
-                data={messages}
-                renderItem={renderMessage}
-                keyExtractor={(_, index) => index.toString()}
-                style={styles.chatContainer}
-            />
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    value={textInput}
-                    onChangeText={setTextInput}
-                    placeholder="Type your message here..."
-                />
-                <Button title="Send" onPress={handleSend} />
-            </View>
-        </KeyboardAvoidingView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                style={{ flex: 1 }}
+                keyboardVerticalOffset={80}
+            >
+                {messageList}
+                {/* {isLoaded && <NBText>Waiting...</NBText>} */}
+                {isLoaded && (
+                    <Text>
+                        <ActivityIndicator size="small" color="#000" />{' '}
+                    </Text>
+                )}
+                <VStack space={2} px={4} mb={4}>
+                    <HStack>
+                        <Input
+                            w="80%"
+                            mx={3}
+                            value={textInput}
+                            onChangeText={setTextInput}
+                            placeholder="Type your message here..."
+                        />
+                        <NBButton onPress={sendMessage} px={5}>
+                            <NBText>Send</NBText>
+                        </NBButton>
+                    </HStack>
+                </VStack>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     );
 };
 
-export default Homepage;
+export default React.memo(Homepage);
