97762ae7e9167176e3ccfb5c5a883156261444cd
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2023-11-04 11:08:58 +0900

    Update: ダークモード対応とプラットフォーム別スタイルの調整

--- src/components/CustomMenu.tsx
+++ src/components/CustomMenu.tsx
@@ -1,20 +1,29 @@
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
-import { Text, View } from 'react-native';
+import { Box, Center, Text, VStack } from 'native-base';
+import { Platform } from 'react-native';
+import AnimatedScreen from './AnimatedScreen';
+import ThemeToggle from './ThemeToggle';
 
 const CustomizeMenu = (props: any): JSX.Element => {
     return (
-        <DrawerContentScrollView {...props}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Custom Header</Text>
-            </View>
-            <DrawerItemList {...props} />
-            {/* Add additional custom items */}
-            <DrawerItem label="Help" onPress={() => console.log('Help pressed')} />
-            {/* You can also add other custom components and buttons here */}
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Custom Drawer Footer</Text>
-            </View>
-        </DrawerContentScrollView>
+        <AnimatedScreen>
+            <DrawerContentScrollView {...props}>
+                <VStack space={3} my={4}>
+                    <Center>
+                        <Text>Custom Header</Text>
+                    </Center>
+                    <DrawerItemList {...props} />
+                    {/* Add additional custom items */}
+                    <DrawerItem label='Help' onPress={() => console.log('Help pressed')} />
+                    {/* You can also add other custom components and buttons here */}
+                </VStack>
+            </DrawerContentScrollView>
+            <Box>
+                <Center py={Platform.OS === 'ios' ? 12 : 6}>
+                    <ThemeToggle />
+                </Center>
+            </Box>
+        </AnimatedScreen>
     );
 };
 
