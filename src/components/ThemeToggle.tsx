cd663167488950a5544cd3536d3198f30deb0413
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2023-11-04 10:46:28 +0900

    Use: useCallbackを使用してテーマ切替処理を最適化

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,13 +1,17 @@
 import { HStack, Switch, Text, useColorMode } from 'native-base';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 const ThemeToggle = (): JSX.Element => {
     const { colorMode, toggleColorMode } = useColorMode();
 
+    const handleToggle = useCallback(() => {
+        toggleColorMode();
+    }, [toggleColorMode]);
+
     return (
         <HStack space={2} alignItems='center'>
             <Text>Dark</Text>
-            <Switch isChecked={colorMode === 'light'} onToggle={toggleColorMode} />
+            <Switch isChecked={colorMode === 'light'} size='md' onToggle={handleToggle} />
             <Text>Light</Text>
         </HStack>
     );
